962ec4a26b4d01b73ae6f68c2e21736e0d79196d
MKandikova <mkandikova04@gmail.com>
2026-03-24 00:36:30 -0400

    Wiki pages added

--- includes/header.php
+++ includes/header.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Online Bookstore</title>
+        <title>MKJM Bookstore</title>
 
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="/WWW/style.css">
     </head>
     <body>
-
+    <!-- Navigation -->
     <div class="navbar">
         <div class="nav-left">
             <form action="books.php" method="GET">
@@ -17,10 +17,25 @@
             </form>
         </div>
         <div class="nav-right">
-            <a href="index.php">Home</a>
-            <a href="books.php">Books</a>
-            <a href="cart.php">Cart</a>
-            <a href="login.php">Login</a>
+            <a href="/WWW/index.php">Home</a>
+            <!-- Wiki pages -->
+            <div class="dropdown">
+                <button class="dropbtn">Wiki</button>
+                <div class="dropdown-content">
+                    <a href="/WWW/WikiPages/readguide.php">Read Guide</a>
+                    <a href="/WWW/WikiPages/authors.php">Famous Authors</a>
+                    <a href="/WWW/WikiPages/genres.php">Genres</a>
+                    <a href="/WWW/WikiPages/bookrec.php">Book Recommendations</a>
+                    <a href="/WWW/WikiPages/booktomovie.php">Book-to-Movie Adaptations</a>
+                </div>
+            </div>
+            <a href="/WWW/books.php">Books</a>
+            <a href="/WWW/cart.php">Cart</a>
+            <a href="/WWW/login.php">Login</a>
         </div>
+    </div>
+
+    <div class="header-box">
+        <h1>The MKJM Bookstore</h1>
     </div>
 </html>
--- index.php
+++ index.php
@@ -1,22 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<?php include 'includes/header.php'; ?>
 
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <?php include 'includes/header.php'; ?>
-    <div class="header-box">
-        <h1>ONLINE BOOKSTORE</h1>
+<!-- <link rel="stylesheet" href="style.css"> -->
+
+    <script src="/WWW/js/script.js"></script>
+    <!-- Recommending best series to read on front page -->
+    <div class="best-series">
+        <h2>BEST SERIES</h2>
+
+        <div class="series-grid">
+            <div class="series-card">
+                <img src="IMAGE">
+                <h3>SERIES ONE</h3>
+            </div>
+
+            <div class="series-card">
+                <img src="IMAGE">
+                <h3>SERIES TWO</h3>
+            </div>
+
+            <div class="series-card">
+                <img src="IMAGE">
+                <h3>SERIES THREE</h3>
+            </div>
+        </div>
     </div>
-
-    <p>This website will be about an online bookstore.</p>
-
-    <script src="script.js"></script>
-
+    <!-- About Us Section -->
+    <div class="about">
+        <h2>About Us</h2>
+        <p>We are an online bookstore dedicated to bringing
+            you the best book series, from fantast to mystery
+            and everything in between.
+        </p>
+    </div>
+    <!-- Find Us on the google maps -->
     <div class="map-container">
         <h2>Find Us</h2>
         <iframe 
@@ -28,5 +44,5 @@
             loading="lazy">
         </iframe>
     </div>
-</body>
-</html>
+
+<?php include 'includes/footer.php'; ?>
